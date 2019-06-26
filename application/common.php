6fa333e98bbd0485a48b5ca3f235bb4b502002de
chengzh <289822055@qq.com>
2019-06-26 15:40:38 +0800

    111

---
+++ application/common.php
@@ -0,0 +1,109 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 流年 <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用公共文件
+function request_curl_post_fun($url, $post_data, $timout = 10)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    // new -- begin
+
+    // 当传输速度小于CURLOPT_LOW_SPEED_LIMIT时(bytes/sec)，PHP会根据CURLOPT_LOW_SPEED_TIME来判断是否因太慢而取消传输。
+    curl_setopt($ch, CURLOPT_LOW_SPEED_LIMIT, 2000);
+
+    //  当传输速度小于CURLOPT_LOW_SPEED_LIMIT时(bytes/sec)，PHP会根据CURLOPT_LOW_SPEED_TIME来判断是否因太慢而取消传输。
+    curl_setopt($ch, CURLOPT_LOW_SPEED_TIME, 3000);
+
+    // 不是CURLCLOSEPOLICY_LEAST_RECENTLY_USED就是CURLCLOSEPOLICY_OLDEST，还存在另外三个CURLCLOSEPOLICY_，但是cURL暂时还不支持。
+    //curl_setopt($ch, CURLOPT_CLOSEPOLICY, CURLCLOSEPOLICY_OLDEST);
+
+    // 在发起连接前等待的时间，如果设置为0，则无限等待。以秒为单位 (CURLOPT_CONNECTTIMEOUT_MS -- 毫秒)
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 8);
+
+    // 允许的最大连接数量，超过是会通过CURLOPT_CLOSEPOLICY决定应该停止哪些连接。
+    curl_setopt($ch, CURLOPT_MAXCONNECTS, 1000);
+
+
+    // 设置cURL允许执行的最长秒数。
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timout);
+
+    // 在完成交互以后强迫断开连接，不能重用。
+    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+
+
+    // -- 设定为不验证证书和host。
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    // --
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+
+    // new -- end
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+function request_curl_get_fun($url, $paramsstr = '')
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url . $paramsstr);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    // new -- begin
+
+    // -- 设定为不验证证书和host。
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    // --
+
+    curl_setopt($ch, CURLOPT_LOW_SPEED_LIMIT, 4000); //
+    curl_setopt($ch, CURLOPT_LOW_SPEED_TIME, 4000); //
+    // curl_setopt($ch, CURLOPT_CLOSEPOLICY, CURLCLOSEPOLICY_OLDEST);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 6); // 尝试连接等待的时间，以秒为单位。如果设置为0，则无限等待。
+    curl_setopt($ch, CURLOPT_MAXCONNECTS, 1000); // 允许的最大连接数量
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    // 在完成交互以后强迫断开连接，不能重用。
+    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+
+    // new -- end
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+function  isImg($fileName)
+{
+    $file     = fopen($fileName, "rb");
+    $bin      = fread($file, 2);  // 只读2字节
+
+    fclose($file);
+    $strInfo  = @unpack("C2chars", $bin);
+    $typeCode = intval($strInfo['chars1'].$strInfo['chars2']);
+    $fileType = '';
+
+    if($typeCode == 255216 /*jpg*/ || $typeCode == 7173 /*gif*/ || $typeCode == 13780 /*png*/)
+    {
+        return $typeCode;
+    }
+    else
+    {
+        // echo '"仅允许上传jpg/jpeg/gif/png格式的图片！';
+        return false;
+    }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,291 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public $root_dir = "";
+    public $sel_dir = "";
+
+    public function initialize()
+    {
+
+        //$this->root_dir = "E:/phpstudy/PHPTutorial/WWW/filemanager";
+        $this->root_dir = $_SERVER['DOCUMENT_ROOT'].'/root';
+        $this->sel_dir = cookie('path');
+
+
+    }
+
+    public function index()
+    {
+        $path = $this->root_dir.$this->sel_dir;
+        //dump(date("Y-m-d H:i:s",filemtime($path)));
+
+        //dump($this->xm_mkdir('www/czh/111',$this->root_dir,true));
+        return $this->fetch();
+    }
+
+    public function get_scandir(){
+
+        $sel_dir = input("path");
+        if($sel_dir){
+            $this->sel_dir = $sel_dir;
+        }
+
+
+        $last = input("last");
+        if($last){
+            if($this->sel_dir != "/"){
+                $this->sel_dir = substr($this->sel_dir,0,strripos($this->sel_dir,"/"));
+            }
+        }
+
+        if($this->sel_dir == ""){
+            $this->sel_dir = "/";
+        }
+
+        cookie('path', $this->sel_dir);
+
+
+        $html = $this->xm_scandir();
+
+        return json(['code'=>0,"data"=>$html,"path"=>$this->sel_dir]);
+    }
+
+
+    public function add_dir(){
+
+        if(input("post.")){
+            $this->sel_dir = input("post.path");
+
+            if($this->sel_dir == ""){
+                $this->sel_dir = "/";
+            }
+
+            $dirname = input("post.dirname");
+
+            if(is_dir($this->root_dir.$this->sel_dir.'/'.$dirname)){
+                return json(['code'=>500,"msg"=>"指定目录已存在！"]);
+            }
+
+
+            $res = $this->xm_mkdir('/'.$dirname,$this->root_dir.$this->sel_dir);
+            if($res){
+                return json(['code'=>200,"msg"=>"目录创建成功！","path"=>$this->sel_dir]);
+            }else{
+                return json(['code'=>500,"msg"=>"目录创建失败！"]);
+            }
+
+        }else{
+            return false;
+        }
+    }
+
+    public function upload_file()
+    {
+        if(input("post.")){
+            $file = request()->file('file');
+
+            $path = input("post.path");
+
+            if($path == ""){
+                $path = "/";
+            }
+
+            $movepath = $this->root_dir.$path;
+
+
+            $info = $file->validate(['size'=>1024*1024*50])->move($movepath,'');
+
+            if($info){
+                return json(['code'=>0,"msg"=>"文件上传成功！"]);
+            }else{
+                return json(['code'=>500,"msg"=>"文件上传失败！"]);
+            }
+        }else{
+            return false;
+        }
+    }
+
+    public function editfile()
+    {
+        if(input("post.")){
+
+            $path = input("post.path");
+            $old_name = input("post.old_name");
+            $new_name = input("post.new_name");
+
+            $old_path = $this->root_dir.$path.$old_name;
+            $new_path = $this->root_dir.$path.$new_name;
+            //dump($old_path);
+            //dump($new_path);
+
+            $res = $this->xm_editfile($old_path,$new_path);
+
+            return json($res);
+        }
+    }
+
+    public function delfile()
+    {
+        if(input("post.")){
+
+            $path = input("post.path");
+            $name = input("post.name");
+
+            $paths = $this->root_dir.$path.$name;
+
+            $res = $this->xm_deldir($paths);
+
+            return json($res);
+        }
+    }
+
+    public function download()
+    {
+        if(input("_m")){
+            $_m = input('_m');
+            $filename = base64_decode($_m);
+            if(file_exists($filename)){
+
+                $fname = substr($filename,strripos($filename,'/')+1);
+
+                header( "Content-Disposition:  attachment;  filename=".$fname); //告诉浏览器通过附件形式来处理文件
+                header('Content-Length: ' . filesize($filename)); //下载文件大小
+                readfile($filename);  //读取文件内容
+
+                exit;
+            }
+        }
+
+        abort("404");
+    }
+
+    /*删除文件，根据路径*/
+    public function xm_deldir($dir) {
+        //先删除目录下的文件：
+        if(is_dir($dir)){
+            $dh=opendir($dir);
+            while ($file=readdir($dh)) {
+                if($file!="." && $file!="..") {
+                    $fullpath=$dir."/".$file;
+                    if(!is_dir($fullpath)) {
+                        unlink($fullpath);
+                    } else {
+                        $this->xm_deldir($fullpath);
+                    }
+                }
+            }
+            closedir($dh);
+            //删除当前文件夹：
+            if(rmdir($dir)) {
+                $return['code']='200';
+                $return['msg']='删除成功';
+            } else {
+                $return['code']='500';
+                $return['msg']='删除失败';
+            }
+        }else{
+            if(unlink($dir)) {
+                $return['code']='200';
+                $return['msg']='删除成功';
+            } else {
+                $return['code']='500';
+                $return['msg']='删除失败';
+            }
+        }
+
+
+        return $return;
+    }
+
+    /*修改文件名*/
+    public function xm_editfile($old_path,$new_path)
+    {
+        if(file_exists($new_path)==false){
+            if (rename($old_path, $new_path))//修改目录
+            {
+                $return['code']='200';
+                $return['msg']='修改成功';
+            }
+            else
+            {
+                $return['code']='500';
+                $return['msg']='修改失败';
+            }
+        }else{
+            $return['code']='500';
+            $return['msg']='相同名称已存在';
+        }
+
+        return $return;
+    }
+
+    public function xm_mkdir($name,$path,$recursive=false){
+
+        if(is_dir($path)){
+
+            return @mkdir($path.$name,644,$recursive);
+
+        }else{
+
+            return false;
+        }
+
+    }
+
+
+    public function xm_scandir()
+    {
+        $dir = $this->root_dir.$this->sel_dir;
+        if(is_dir($dir)){
+            $child_dirs = scandir($dir);
+            //$html = "";
+            $html = [];
+            foreach($child_dirs as $child_dir){
+                if($child_dir != '.' && $child_dir != '..'){
+                    $sel_dir = $this->sel_dir=="/"?"":$this->sel_dir;
+                    /*$html .= "<tr data-path='".$sel_dir."/' data-name='".$child_dir."'>";
+                    if(is_dir($dir.'/'.$child_dir)){
+                        $html .= "<td><div class='getfile' data-path='".$sel_dir."/".$child_dir."'><img src='/static/images/filedir.png' alt=''> ".$child_dir."</div></td>";
+                    }else{
+                        $html .= "<td><img src='/static/images/fileico.png' alt=''> ".$child_dir."</td>";
+                    }
+
+                    $html .= "<td>".date("Y-m-d H:i:s",filemtime($dir.'/'.$child_dir))."</td>";
+                    $html .= "<td>".substr(base_convert(fileperms($dir.'/'.$child_dir), 10, 8), -3)."</td>";
+                    $html .= "<td></td>";
+
+                    $html .= "</tr>";*/
+                    $tmp['path'] =$sel_dir."/";
+                    $tmp['name'] =$child_dir;
+                    if(is_dir($dir.'/'.$child_dir)){
+                        $tmp['filename']= "<div class='getfile' data-path='".$sel_dir."/".$child_dir."'><img src='/static/images/filedir.png' alt=''> ".$child_dir."</div>";
+                        $tmp['operator']= '<div class="opermt"><span class="edname" data-filename="'.$child_dir.'" data-filepath="'.$this->sel_dir.'/">重命名</span> | <span class="deldirfunc" data-filename="'.$child_dir.'" data-filepath="'.$this->sel_dir.'/">删除</span></div>';
+
+                    }else{
+                        $tmp['size']= round(filesize($dir.'/'.$child_dir)/1024,2)."KB";
+
+                        if(isImg($dir.'/'.$child_dir)){
+                            $tmp['filename']= "<img width='32' onclick='previewImg(this)' src='/root/".$this->sel_dir.'/'.$child_dir."' alt=''> ".$child_dir;
+                        }else{
+                            $tmp['filename']= "<img src='/static/images/fileico.png' alt=''> ".$child_dir;
+                        }
+                        $tmp['operator']= '<div class="opermt"><span class="edname" data-filename="'.$child_dir.'" data-filepath="'.$this->sel_dir.'/">重命名</span> | <a target="_blank" href="'.url('download')."?_m=".base64_encode($dir.'/'.$child_dir).'">下载</a> | <span class="deldirfunc" data-filename="'.$child_dir.'" data-filepath="'.$this->sel_dir.'/">删除</span></div>';
+
+                    }
+
+                    $tmp['updatetime']= date("Y-m-d H:i:s",filemtime($dir.'/'.$child_dir));
+                    $tmp['role']= substr(base_convert(fileperms($dir.'/'.$child_dir), 10, 8), -3);
+
+                    $html[] = $tmp;
+                }
+            }
+		    return $html;
+	    }else{
+            return '';
+        }
+    }
+}
